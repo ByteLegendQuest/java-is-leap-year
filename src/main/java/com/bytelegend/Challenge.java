407bfd48145c4e08bbac54d7875380807e349239
Birlinha <omenaribeiro13@gmail.com>
2022-07-15 01:36:42 +0000

    Update at 2022-07-15T01:36:42.693906Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -22,6 +22,14 @@ public class Challenge {
      * <p>**Note: `if`/`else` are not allowed; only logical operators are allowed.**
      */
     public static boolean isLeapYear(int year) {
-        return false;
+        if(year % 400 == 0) {
+            return true;
+        } else if (year % 100 == 0) {
+            return true;
+        } else if(year % 4 == 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
