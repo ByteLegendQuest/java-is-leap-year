17541478d38c70c970eb61601eb5a0d1abc2c818
jenny-2233 <822204051@qq.com>
2022-11-27 13:15:12 +0000

    Update at 2022-11-27T13:15:12.325061Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -23,11 +23,8 @@ public class Challenge {
      */
     public static boolean isLeapYear(int year) {
 
-        if(year%4==0 &&year%100!=0 || year%400==0){
-            return true;
-        }else{
-            return false;
-        }
+        return (year%4==0 &&year%100!=0 || year%400==0) ? true : false;
+      
 
     }
 
